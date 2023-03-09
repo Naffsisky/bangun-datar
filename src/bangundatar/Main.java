3a03a3181041ae2831f27be0a192433a8b6362b5
naffsvn <naffsvn@protonmail.com>
2023-03-09 07:52:33 +0700

    Rev 1.1

--- src/bangundatar/Main.java
+++ src/bangundatar/Main.java
@@ -22,6 +22,8 @@ public class Main {
         Rectangle rec = new Rectangle();
         Circle circ = new Circle();
         Elipse el = new Elipse();
+        Kubus getVolume = new Kubus();
+        Balok kotak = new Balok();
         Scanner input = new Scanner(System.in);
         
         // input
@@ -30,46 +32,64 @@ public class Main {
         System.out.println("2. Rectangle");
         System.out.println("3. Circle");
         System.out.println("4. Elipse");
+        System.out.println("5. Kubus");
+        System.out.println("6. Balok");
         System.out.print("Masukan pilihan: ");
         int pilihan = input.nextInt();
-        if(pilihan == 1){
-            System.out.print("Masukan sisi: ");
-            square.sisi = input.nextInt();
-            square.Luas();
-            square.Keliling();
-            System.out.println("Luas: " + square.Luas());
-            System.out.println("Keliling: " + square.Keliling());
-        }
-        else if(pilihan == 2){
-            System.out.print("Masukan panjang: ");
-            rec.panjang = input.nextInt();
-            System.out.print("Masukan lebar: ");
-            rec.lebar = input.nextInt();
-            rec.Luas();
-            rec.Keliling();
-            System.out.println("Luas: " + rec.Luas());
-            System.out.println("Keliling: " + rec.Keliling());
-        }
-        else if(pilihan == 3){
-            System.out.print("Masukan radius: ");
-            circ.radius = input.nextInt();
-            circ.Luas();
-            circ.Keliling();
-            System.out.println("Luas: " + circ.Luas());
-            System.out.println("Keliling: " + circ.Keliling());
-        }
-        else if(pilihan == 4){
-            System.out.print("Masukan sumbu a: ");
-            el.a = input.nextInt();
-            System.out.print("Masukan sumbu b: ");
-            el.b = input.nextInt();
-            el.Luas();
-            el.Keliling();
-            System.out.println("Luas: " + el.Luas());
-            System.out.println("Keliling: " + el.Keliling());
-        }
-        else{
-            System.out.println("Pilihan tidak ada!");
+        switch (pilihan) {
+            case 1 -> {
+                System.out.print("Masukan sisi: ");
+                square.sisi = input.nextInt();
+                square.Luas();
+                square.Keliling();
+                System.out.println("Luas: " + square.Luas());
+                System.out.println("Keliling: " + square.Keliling());
+            }
+            case 2 -> {
+                System.out.print("Masukan panjang: ");
+                rec.panjang = input.nextInt();
+                System.out.print("Masukan lebar: ");
+                rec.lebar = input.nextInt();
+                rec.Luas();
+                rec.Keliling();
+                System.out.println("Luas: " + rec.Luas());
+                System.out.println("Keliling: " + rec.Keliling());
+            }
+            case 3 -> {
+                System.out.print("Masukan radius: ");
+                circ.radius = input.nextInt();
+                circ.Luas();
+                circ.Keliling();
+                System.out.println("Luas: " + circ.Luas());
+                System.out.println("Keliling: " + circ.Keliling());
+            }
+            case 4 -> {
+                System.out.print("Masukan sumbu a: ");
+                el.a = input.nextInt();
+                System.out.print("Masukan sumbu b: ");
+                el.b = input.nextInt();
+                el.Luas();
+                el.Keliling();
+                System.out.println("Luas: " + el.Luas());
+                System.out.println("Keliling: " + el.Keliling());
+            }
+            case 5 -> {
+                System.out.print("Masukan sisi: ");
+                getVolume.volume = input.nextInt();
+                getVolume.ComputeAndSetVolume();
+                System.out.println("Volume: " + getVolume.ComputeAndSetVolume());
+            }
+            case 6 -> {
+                System.out.print("Masukan panjang: ");
+                kotak.panjang = input.nextInt();
+                System.out.print("Masukan lebar: ");
+                kotak.lebar = input.nextInt();
+                System.out.print("Masukan tinggi: ");
+                kotak.tinggi = input.nextInt();
+                kotak.ComputeAndSetVolume();
+                System.out.println("Volume: " + kotak.getVolume());
+            }
+            default -> System.out.println("Pilihan tidak ada!");
         }
         
     }
